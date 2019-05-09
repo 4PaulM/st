b4d036423ab015d26dd61ad4b2ea0f6e08a3b73b
4PaulM <48378355+4PaulM@users.noreply.github.com>
2019-05-09 17:27:37 +0300

    1

---
+++ get.py
@@ -0,0 +1,34 @@
+import os
+from bottle import route, run, view, redirect, request, static_file
+
+x = {1:"Text"}
+y = 111
+
+@route("/addinfo", method = "POST")
+def info():
+	inf = str(request.POST.inputinfo.strip())
+	key = (max(x.keys()))+1
+	x[key]=inf
+	i = request.POST.info1.strip()
+	y = i
+	return redirect("/")
+
+@route("/site")
+@view("site")
+def logic():
+	return{"infor":x,"id":y}
+
+@route("/api/<k>")
+def delete(k):
+	return {"infor":x,"id":y}
+
+@route("/api/delete/<u>")
+def delete(u):
+	u = int(u)
+	del x[u]
+	return redirect("/")
+
+if os.environ.get('APP_LOCATION') == 'heroku':
+    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+else:
+    run(host='localhost', port=8080, debug=True)
